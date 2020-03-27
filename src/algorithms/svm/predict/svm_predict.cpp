0f58b9a9741889a6b74ae6bd136522613d803222
vnazarov <vladislav.nazarov@intel.com>
2020-03-27 16:36:13 +0300

    Add svm predict bench

---
+++ src/algorithms/svm/predict/svm_predict.cpp
@@ -0,0 +1,124 @@
+/** file svm_predict.cpp */
+/*******************************************************************************
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#include "fixture.hpp"
+
+#include "svm_predict_params.hpp"
+#include "dataset.hpp"
+#include "daal.h"
+#include <iostream>
+#include <typeinfo>
+
+namespace dalbench {
+namespace svm {
+
+namespace daal_svm_train   = daal::algorithms::svm::training;
+namespace daal_svm_predict = daal::algorithms::svm::prediction;
+namespace daal_classifier_train = daal::algorithms::classifier::training;
+namespace daal_classifier_prediction = daal::algorithms::classifier::prediction;
+namespace daal_kernel_function = daal::algorithms::kernel_function;
+
+template <typename DeviceType, typename FPType>
+class SvmPredict : public GetterParamsSvmPredict<FPType>,
+                      public FixtureBatch<daal_svm_predict::Batch<FPType>, DeviceType> {
+public:
+  using GetterParamsSvmPredict<FPType>::params;
+  
+  using AlgorithmType = typename daal_svm_predict::Batch<FPType>;
+
+  SvmPredict(const std::string& name,
+                const typename GetterParamsSvmPredict<FPType>::Params& params_in)
+      : GetterParamsSvmPredict<FPType>(params_in),
+        FixtureBatch<AlgorithmType, DeviceType>(name, params) {}
+
+protected:
+  void set_algorithm() final {
+    this->algorithm_ = std::make_unique<AlgorithmType>(AlgorithmType());
+  }
+
+  void set_input(benchmark::State& state) final {
+    auto x = params.dataset.train().x();
+    auto y = params.dataset.train().y();
+    convert_dataset(y);
+
+    daal_kernel_function::KernelIfacePtr kernel_func;
+    switch (params.kernel_type) {
+      case KernelType::rbf:
+        kernel_func = daal_kernel_function::KernelIfacePtr(new daal_kernel_function::rbf::Batch<FPType>());
+      case KernelType::linear:
+        kernel_func = daal_kernel_function::KernelIfacePtr(new daal_kernel_function::linear::Batch<FPType>());
+    }
+
+    daal_svm_train::Batch<FPType> train_algorithm;
+    const size_t num_cache_size = std::min(x->getNumberOfRows(), 60000ul);
+    train_algorithm.parameter.maxIterations = 100;
+    train_algorithm.parameter.cacheSize = num_cache_size * num_cache_size * sizeof(FPType); 
+    train_algorithm.parameter.accuracyThreshold = 0.1;
+    train_algorithm.parameter.kernel = kernel_func;
+
+    train_algorithm.input.set(daal_classifier_train::data, x);
+    train_algorithm.input.set(daal_classifier_train::labels, y);
+
+    train_algorithm.compute();
+
+    auto train_model = train_algorithm.getResult()->get(daal_classifier_train::model);
+    auto sv = train_model->getSupportVectors();
+    check_sv(sv);
+
+    this->algorithm_->input.set(daal_classifier_prediction::data, x);
+    this->algorithm_->input.set(daal_classifier_prediction::model, train_model);
+    this->algorithm_->parameter.kernel = kernel_func;
+  }
+
+  MetricParams check_result(benchmark::State& state) final {
+    auto x = params.dataset.train().x();
+    auto y = params.dataset.train().y();
+    auto prediction_result = this->algorithm_->getResult();
+    auto y_predict         = prediction_result->get(daal_classifier_prediction::prediction);
+    convert_prediction(y_predict);
+    return Metric<MetricType::Accuracy>::compute_metric(y, y_predict);
+  }
+
+  void convert_dataset(const NumericTablePtr& _dataset) {
+    daal::data_management::BlockDescriptor<FPType> svm_block;
+    _dataset->getBlockOfRows(0, _dataset->getNumberOfRows(), daal::data_management::readWrite, svm_block);
+    FPType *block = svm_block.getBlockPtr();
+    for (size_t i = 0; i < _dataset->getNumberOfRows() * _dataset->getNumberOfColumns(); ++i)
+      if (block[i] == 0) 
+        block[i] = -1;
+    _dataset->releaseBlockOfRows(svm_block);
+  }
+
+  void convert_prediction(const NumericTablePtr& _predict) {
+    daal::data_management::BlockDescriptor<FPType> predict_block;
+    _predict->getBlockOfRows(0, _predict->getNumberOfRows(), daal::data_management::readWrite, predict_block);
+    FPType *block = predict_block.getBlockPtr();
+    for (size_t i = 0; i < _predict->getNumberOfRows() * _predict->getNumberOfColumns(); ++i)
+      block[i] = block[i] >= 0 ? 1 : -1; 
+    _predict->releaseBlockOfRows(predict_block);
+  }
+
+  void check_sv(const NumericTablePtr& sv) {
+    std::cout << sv->getNumberOfRows() << std::endl;
+  }
+};
+
+DAL_BENCH_REGISTER(SvmPredict, CpuDevice, float);
+DAL_BENCH_REGISTER(SvmPredict, CpuDevice, double);
+
+} // namespace svm
+} // namespace dalbench
---
+++ src/algorithms/svm/predict/svm_predict_params.hpp
@@ -0,0 +1,69 @@
+/** file svm_predict_params.hpp */
+/*******************************************************************************
+* Copyright 2020 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#ifndef DALBENCH_PREDICT_SVM_PARAMS_HPP_
+#define DALBENCH_PREDICT_SVM_PARAMS_HPP_
+
+#include "fixture.hpp"
+
+namespace dalbench {
+  enum class KernelType {
+    linear,
+    rbf,
+  };
+
+template <typename FPType>
+class GetterParamsSvmPredict {
+public:
+  struct Params : public CommonAlgorithmParams {
+    Params(const DatasetName& dataset_name,
+           const NumericTableType numeric_table_type,
+           const KernelType kernel_type)
+        : CommonAlgorithmParams(dataset_name, numeric_table_type),
+          kernel_type(kernel_type) {}
+
+    const KernelType kernel_type;
+  };
+
+  GetterParamsSvmPredict(const Params& params) : params(params) {}
+
+  static DictionaryParams<Params> get_params() {
+    return { { "Higgs:Linear:100k",
+               Params(DatasetName("higgs_small_classifier"),
+                      TableType(SyclHomogen, FPType),
+                      KernelType(KernelType::linear)) },
+             { "Higgs:RBF:100k",
+               Params(DatasetName("higgs_small_classifier"),
+                      TableType(SyclHomogen, FPType),
+                      KernelType(KernelType::rbf)) },
+             { "Epsilon:Linear:50k;",
+               Params(DatasetName("epsilion_50k_classifier"),
+                      TableType(SyclHomogen, FPType),
+                      KernelType(KernelType::linear)) },
+             { "Epsilon:Linear:50k;",
+               Params(DatasetName("epsilion_50k_classifier"),
+                      TableType(SyclHomogen, FPType),
+                      KernelType(KernelType::rbf)) },
+    };
+  }
+
+  Params params;
+};
+
+} // namespace dalbench
+
+#endif // DALBENCH_PREDICT_SVM_PARAMS_HPP_
